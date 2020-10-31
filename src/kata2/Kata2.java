03a87848e21e975e559fba59d61d1967234cee1f
victo <victo@DESKTOP-VKVT4U5>
2020-10-31 10:17:22 +0000

    Versión2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -10,9 +10,9 @@ public class Kata2 {
         int[] data ={2,3,5,7,12,1,4,9,4,0,22,6,3,8,15};
         
         Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i = 0; i < data.length ; i++) {
-            histogram.put(data[i], data[i]+1);
-            System.out.println(data[i] + "==>" + histogram.get(data[i]));
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+            System.out.println(key + "==>" + histogram.get(key));
         }
         
     }
