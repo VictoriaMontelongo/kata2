8c795c4ad94345664b5c15a90f50309418324d7d
victo <victo@DESKTOP-VKVT4U5>
2020-10-31 10:41:07 +0000

    Versión4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] data ={2,3,5,7,12,1,4,9,4,0,22,6,3,8,15};
+        Integer[] data ={2,3,5,7,12,1,4,9,4,0,22,6,3,8,15};
         
         Histogram histo = new Histogram(data);
         Map<Integer,Integer> histogr = histo.getHistogram();
